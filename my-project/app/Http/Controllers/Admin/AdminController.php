a0a7bdcac898b7ec56571a4cabab9d7aef00fe5d
ITICFranciscoJimenez <2023_francisco.jimenez@iticbcn.cat>
2023-11-26 22:47:06 +0100

    Corregido reedireccionamiento

--- my-project/app/Http/Controllers/Admin/AdminController.php
+++ my-project/app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class AdminController extends Controller
 {
@@ -17,7 +18,7 @@ class AdminController extends Controller
         if ($postedEmail == $email && $postedPass == $password) {
             return view('Admin.admin');
         } else {
-            return view('signin');
+            return redirect()->route('signin');
         }
     }
 
